c78800c6bff7b4feac1c428af5fa23cb9c38c2ed
Toni Uhlig <matzeton@googlemail.com>
2017-12-18 13:47:15 +0100

    base64 removed

--- base64.h
+++
@@ -1,8 +0,0 @@
-#ifndef BASE64_H
-#define BASE64_H 1
-
-void base64_cleanup(void);
-char *base64_encode(const unsigned char *data, size_t input_length, size_t *output_length);
-unsigned char *base64_decode(const char *data, size_t input_length, size_t *output_length);
-
-#endif
