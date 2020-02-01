5edf3ce72d68b8b65fecd44003af6d011b93185d
Toni Uhlig <matzeton@googlemail.com>
2020-02-01 21:49:46 +0100

    integration test instrumentalizes ptunnel
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -11,12 +11,12 @@ PTUNNEL_BIN=${SRC_ROOT}/src/ptunnel-ng
 TUNNEL_TIMEOUT=25
 timeout --foreground -k1 ${TUNNEL_TIMEOUT} \
     valgrind --error-exitcode=1 --exit-on-first-error=yes \
-    "${PTUNNEL_BIN}" -v4 -r127.0.0.1 -R3000 >/dev/null &
+    "${PTUNNEL_BIN}" -v4 -r127.0.0.1 -R3000 -l4000 >/tmp/ptunnel-server.log &
 PTUNNEL_SERVER_PID=$!
 
 timeout --foreground -k1 ${TUNNEL_TIMEOUT} \
     valgrind --error-exitcode=1 --exit-on-first-error=yes \
-    "${PTUNNEL_BIN}" -v4 -p127.0.0.1 -r127.0.0.1 -R3000 >/dev/null &
+    "${PTUNNEL_BIN}" -v4 -p127.0.0.1 -r127.0.0.1 -R3000 -l4000 >/tmp/ptunnel-client.log &
 PTUNNEL_CLIENT_PID=$!
 
 timeout --foreground -k1 ${TUNNEL_TIMEOUT} \
@@ -26,7 +26,7 @@ DATA_SERVER_PID=$!
 sleep 3
 DATA_TIMEOUT=20
 timeout --foreground -k1 ${DATA_TIMEOUT} \
-    sh -c "dd if=/dev/urandom bs=8192 | nc 127.0.0.1 3000" || true
+    sh -c "dd if=/dev/urandom bs=8192 | nc 127.0.0.1 4000" || true
 
 wait ${PTUNNEL_SERVER_PID} || true
 wait ${PTUNNEL_CLIENT_PID} || true
